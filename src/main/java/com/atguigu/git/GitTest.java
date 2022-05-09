25f8be946b1234cfacbea4bb91c30f097905cc65
kzl <kzl@qq.com>
2022-05-09 20:46:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author kzl
+ * @create 2022-05-09-20:43
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
